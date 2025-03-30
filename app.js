b4e1db10616727c04c932d7d39c506d511c62c2c
Shreeya Prasanna <134350531+pshreeya@users.noreply.github.com>
2025-03-30 09:42:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
+/*
+  Name: Mars Exploration App
+  Purpose: To see the latest photos from Spirit, Curiority, and Opportunity Mars Rovers. 
+  Author:  Shreeya Prasanna
+  Created: 15-Dec-2020
+  Updated: 30-March-2025
+  Visit 'https://api.nasa.gov/' to generate API Key
+*/
 
 var data;
+//Navigate to Spirit Rover's photo screen
 onEvent("Spirit", "click", function( ) {
   setScreen("SpiritPhotos");
 });
+// Navigate back to Home screen from Spirit photos
 onEvent("buttonhome2", "click", function( ) {
   setScreen("Home");
 });
 
-var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=_______________________________";
+// API request for Spirit Rover images
+var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=DEMOKEY"; //update the query using your API Key
 var currentImage= 0;
 function processspiritData(status,type,content) {
-  data= JSON.parse(content);
-  updatespiritImage();
+  data= JSON.parse(content); 
+  updatespiritImage(); //update UI with the first image
 }
+
+// Updates the displayed image information 
 function updatespiritImage () {
   setText ("infomars1","Camera Name:"+ data.latest_photos[currentImage].camera.full_name + "\nDate Taken:" + data.latest_photos [currentImage].earth_date);
   setImageURL("imagemars1",data.latest_photos[currentImage].img_src);
@@ -21,16 +34,18 @@ function updatespiritImage () {
 
 startWebRequest(url,processspiritData); 
 
+//navigates to show the next Spirit Rover image
 onEvent("next1","click", function () {
   if (currentImage==data.latest_photos.length - 1) {
-  currentImage=0;
+  currentImage=0; 
   }
   else {
     currentImage++;
   }
   updatespiritImage();
 });
- 
+
+//navigates to show the previous Spirit Rover image
 onEvent("back1","click", function () {
   if (currentImage==0) {
   currentImage=data.latest_photos.length - 1;
@@ -41,6 +56,7 @@ onEvent("back1","click", function () {
   updatespiritImage();
 });
 
+//the below code follows the same structure to show images from both Curiosity and Opportunity rovers
 
 onEvent("Curiosity", "click", function( ) {
   setScreen("Curiosityphotos");
@@ -48,7 +64,7 @@ onEvent("Curiosity", "click", function( ) {
 onEvent("buttonhome1", "click", function( ) {
   setScreen("Home");
 });
-var url1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=_________________________________";
+var url1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=DEMOKEY";
 var data1;
 var currentImage1= 0;
 function processcuriosityData(status,type,content) {
@@ -91,7 +107,7 @@ onEvent("buttonhome3", "click", function( ) {
   setScreen("Home");
 });
 
-var url2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=_______________________________";
+var url2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=DEMOKEY";
 var data2;
 var currentImage2= 0;
 function processopportunityData(status,type,content) {
